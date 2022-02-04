4658f5da727fa9f80d7c70082ef4e76f3eacc32f
JackArmstrong22 <>
2022-02-04 17:10:03 +0000

    Explore1 working

--- Explore1.cpp
+++ Explore1.cpp
@@ -14,9 +14,14 @@ int main() {
     Point b;
     b.x = 4.5;
     b.y = 7.8;
+    // Third instance of Point, named changes
+    Point c;
+    c.x = 7.2;
+    c.y = 7.0;
 
     cout << "Point a: x = " << a.x << ", y = " << a.y << endl;
     cout << "Point b: x = " << b.x << ", y = " << b.y << endl;
+    cout << "Point c: x = " << c.x << ", y = " << c.y << endl;
 
     // Add your changes here.
 
